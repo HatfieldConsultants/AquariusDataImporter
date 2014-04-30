7703814aefb345fe23e10e869cf7523633b843c2
Daniel <luzhanxun.daniel@gmail.com>
2014-04-29 18:10:06 -0700

    OptimumImportTaskHandler stable improve

--- AquariusDataImporter/Source/Hatfield.AquariusDataImporter.Core/TaskHandlers/OptimumImportTaskHandler.cs
+++ AquariusDataImporter/Source/Hatfield.AquariusDataImporter.Core/TaskHandlers/OptimumImportTaskHandler.cs
@@ -1,19 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using Hatfield.AquariusDataImporter.Core.Models.Optimum;
-using Hatfield.AquariusDataImporter.Core.Models;
-using log4net;
 using Hatfield.AquariusDataImporter.Core.Aquarius;
 using Hatfield.AquariusDataImporter.Core.Helpers;
+using Hatfield.AquariusDataImporter.Core.Models;
+using Hatfield.AquariusDataImporter.Core.Models.Optimum;
+using log4net;
 
 namespace Hatfield.AquariusDataImporter.Core.TaskHandlers
 {
     public class OptimumImportTaskHandler : IImportTaskHandler
     {
-        private static readonly ILog log = LogManager.GetLogger("Application");        
+        private static readonly ILog log = LogManager.GetLogger("Application");
 
         private IAquariusAdapter _aquariusAdapter;
 
@@ -21,7 +19,6 @@ namespace Hatfield.AquariusDataImporter.Core.TaskHandlers
         {
             _aquariusAdapter = aquariusAdapter;
         }
-
 
         public ImportResult Import(IImportable task, DateTime? lastImportTime, int interval)
         {
@@ -51,24 +48,38 @@ namespace Hatfield.AquariusDataImporter.Core.TaskHandlers
 
                 var dataLogger = OptimumDataDownloadHelper.GetDataLoggersBySiteName(castedTask.StationName);
                 var inputInfoOfDataLogger = OptimumDataDownloadHelper.GetInputInfoOfDataLogger(dataLogger);
+
                 foreach (var parameter in castedTask.Parameters)
                 {
-                    var matchedInputInfo = inputInfoOfDataLogger.Where(x => x.Label == parameter.OptimumInputName).First();
-                    var csvString = OptimumDataDownloadHelper.FetchCSVForInput(dataLogger, matchedInputInfo, startTime, endTime);
+                    try
+                    {
+                        var matchedInputInfo = inputInfoOfDataLogger.Where(x => x.Label == parameter.OptimumInputName).First();
+                        var csvString = OptimumDataDownloadHelper.FetchCSVForInput(dataLogger, matchedInputInfo, startTime, endTime);
 
-                    ImportCSVDataToAquarius(parameter, csvString);
+                        ImportCSVDataToAquarius(parameter, csvString);
+                    }
+                    catch (Exception ex)
+                    {
+                        importMessageBuilder.Append("Error: " + ex.Message);
+                        errorThredHold--;
+                        if (errorThredHold < 0)
+                        {
+                            throw ex;
+                        }
+                        continue;
+                    }
                 }
 
                 importMessageBuilder.Append("Import data success");
-                
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 importMessageBuilder.Append("Error: " + ex.Message);
                 errorThredHold--;
-                if(errorThredHold < 0)
+                if (errorThredHold < 0)
                 {
-                    return new ImportResult { 
+                    return new ImportResult
+                    {
                         Success = false,
                         LogMessage = importMessageBuilder.ToString()
                     };
@@ -82,7 +93,6 @@ namespace Hatfield.AquariusDataImporter.Core.TaskHandlers
             };
         }
 
-
         private void ImportCSVDataToAquarius(OptimumParameter optimumParameter, string csvString)
         {
             var aquariusId = _aquariusAdapter.GetDataSetIdByIdentifier(optimumParameter.AquariusDatasetIdentifier);
@@ -91,13 +101,13 @@ namespace Hatfield.AquariusDataImporter.Core.TaskHandlers
 
             var dateTimeColumnIndex = headers.IndexOf(Constants.DateTimeHeaderText);
 
-            if(dateTimeColumnIndex == -1)
+            if (dateTimeColumnIndex == -1)
             {
                 throw new IndexOutOfRangeException("Can not find date time header in the download csv for " + optimumParameter.AquariusDatasetIdentifier);
             }
             var valueColumnIndex = headers.IndexOf(Constants.MeasureMentHeaderText);
 
-            if(valueColumnIndex == -1)
+            if (valueColumnIndex == -1)
             {
                 throw new IndexOutOfRangeException("Can not find measurement header in the download csv for " + optimumParameter.AquariusDatasetIdentifier);
             }
@@ -111,15 +121,12 @@ namespace Hatfield.AquariusDataImporter.Core.TaskHandlers
                     var actualValue = string.IsNullOrEmpty(values[valueColumnIndex]) ? null : (double?)double.Parse(values[valueColumnIndex]);
 
                     _aquariusAdapter.PersistTimeSeriesData(aquariusId, AquariusHelper.ConstructAquariusInsertString(dateTime, 0, actualValue));
-                } 
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw ex;
             }
-                      
-            
-
 
         }
     }
